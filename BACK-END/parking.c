3426dc88e43f161f6591162fcfb88bd06478fb9f
GhostironmanRVG2 <GhostironmanRVG2@gmail.com>
2021-11-13 12:48:18 +0000

    KJ

--- BACK-END/parking.c
+++ BACK-END/parking.c
@@ -3,71 +3,95 @@
 //OBJETO PARKING QUE VAI SER ARMAZENADO NUM ARRAY TRIDIMENCIONAL
 typedef struct parking
 {
- char matricula[8];
+ char matricula[9];
 }parking;
+
+
+
+
+
 //DECLARACAO DA FUNCAO COM OS PARAMETROS
 void Estacionar(char matricula[],int estacionamento, struct parking parque[][2][2]);
 
+
+
 //FUNCAO ESTACIONAR PARA ESTACIONAR TENDO UMA MATRICULA ,NMR DE ESTACIONAMENTO E O PARQUE
 void Estacionar(char matricula[],int estacionamento, struct parking parque[][2][2]){
+
 //ACIONAR UM METODO CONSOANTE O ESTACIONAMENTO
 switch (estacionamento)
 {
 case 1:
 //CASO SEJA O ESTACIONAMENTO 1 , ARMAZENAR ENTAO NAS COORDENADAS 0 0 0 OS DADOS!
-strcpy(parque[0][0][0].matricula,matricula);    
+strcpy(parque[0][0][0].matricula,matricula);
 break;
+
 case 2:
-   
+strcpy(parque[0][0][1].matricula,matricula);   
 break;
 
 case 3:
-    
+strcpy(parque[0][1][0].matricula,matricula);   
 break;
+
 case 4:
-    
+strcpy(parque[0][1][1].matricula,matricula);   
+//PRIMEIRO ANDAR FEITO 
 break;
+
 case 5:
-
+strcpy(parque[1][0][0].matricula,matricula);
 break;
+
 case 6:
+strcpy(parque[1][0][1].matricula,matricula);
 break;
-case 7:
-    
-break;
-case 8:
-    
-break;
-case 9:
-    
-break;
-case 10:
 
+case 7:
+strcpy(parque[1][1][0].matricula,matricula);  
 break;
+
+case 8:
+strcpy(parque[1][1][1].matricula,matricula); 
+//PISO 2
+break;
+
+case 9:
+strcpy(parque[2][0][0].matricula,matricula);
+break;
+
+case 10:
+strcpy(parque[2][0][1].matricula,matricula);
+break;
+
 case 11:
-    
+strcpy(parque[2][1][0].matricula,matricula); 
 break;
 
 case 12:
-    
+strcpy(parque[2][1][1].matricula,matricula);
+//PISO 3
 break;
+
 case 13:
-    
+strcpy(parque[3][0][0].matricula,matricula);
 break;
 
 case 14:
-    
+strcpy(parque[3][0][1].matricula,matricula);
 break;
+
 case 15:
-    
+strcpy(parque[3][1][0].matricula,matricula);    
 break;
 
 case 16:
-    
+strcpy(parque[3][1][1].matricula,matricula);
+//PISO 4    
 break;
 
 default:
-
+printf("Erro");
 break;
 }
 
--- start.c
+++ start.c
@@ -5,10 +5,43 @@
 int main()
 {
 //INICIALIZACAO DO NOSSO PARQUE    
-parking parque[4][2][2];   
+parking parque[4][2][2];
+int piso;
+int linha=0;
+int coluna=0;
+char m[9];   
+printf("estacionamento?\n");
+scanf("%d",&piso);
+printf("Matricula?\n");
+scanf("%s",&m);
+printf("%s \n",m);
+Estacionar(m,piso,parque);
+//CUSPIR O NOSSO ARRAY
+for (int i = 0; i <=3; i++)
+{   
+    //QUANDO VOLTA VOLTA AQUI MUDA O PISO
+    linha=0;
+    for (linha;linha<=1;linha++)
+    {
+    //LINHA
+    coluna=0;
+    
+    for (coluna;coluna<=1;coluna++)
+    {
+    //COLUNA
+    printf("[%d][%d][%d] matricula:%s\n",i,linha,coluna,parque[i][linha][coluna].matricula);  
+       
+           
+    }
+    
+     
 
-Estacionar("10-IO-OP",1,parque);
-printf("%s",parque[0][0][0].matricula);
+
+    }
+    
+    
+}
+
 
 
 
